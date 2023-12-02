d7919ce352b21b4f6fd736e3ab68ea94e946b4fa
AMANBH0207 <amanbhardwaj052@gmail.com>
2023-12-02 09:36:48 +0530

    goodgamebeers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [beers, setBeers] = useState([]);
+  const [originalBeers, setOriginalBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.punkapi.com/v2/beers');
+      const data = await response.json();
+      setBeers(data);
+      setOriginalBeers(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSearch = () => {
+    const filteredBeers = originalBeers.filter((beer) =>
+      beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setBeers(filteredBeers);
+  };
+
+  const resetSearch = () => {
+    setSearchTerm('');
+    setBeers(originalBeers);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search by beer name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+        <button onClick={resetSearch}>Reset</button>
+      </div>
+      <div className="beer-list">
+        {beers.map((beer) => (
+          <div key={beer.id} className="beer-card">
+            <img src={beer.image_url} alt={beer.name} />
+            <h3>{beer.name}</h3>
+            <p>{beer.tagline}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
